9429d0255d41434e5b74a7ee7069560088aa2bcd
Evan Reid <evanr777@protonmail.com>
2021-07-17 14:30:40 -0700

    Create web3 hook

--- src/App.js
+++ src/App.js
@@ -7,60 +7,11 @@ import BlogPage from './pages/BlogPage/BlogPage';
 import ReadPage from './pages/ReadPage/ReadPage';
 import React, { useState, useEffect } from "react";
 import Web3 from 'web3';
+import { useWeb3 } from './hooks/web3'
 
 const App = () => {
-  const [account, setAccount] = useState('0x0')
-  const [isConnected, setIsConnected] = useState(false)
-  const [loading, setLoading] = useState(true)
 
-  const loadWeb3 = async () => {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-
-      const accounts = await window.web3.eth.getAccounts()
-
-      setAccount(accounts[0])
-      setIsConnected(true)
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected.')
-    }
-  }
-
-  loadWeb3()
-  
-  const setupWalletListeners = () => {
-    window.ethereum.on('accountsChanged', (accounts) => {
-      //Time to reload your interface with accounts[0]!
-      if (accounts.length > 0) {
-        setAccount(accounts[0])
-        setIsConnected(true)
-      } else {
-        setAccount('0x0')
-        setIsConnected(false)
-      }
-    })
-  }
-
-  const loadAccounts = async () => {
-    try {
-      const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      setAccount(accounts[0])
-      setIsConnected(true)
-    } catch (err) {
-      setIsConnected(false)
-    }
-  }
-
-  useEffect(() => {
-		setupWalletListeners()
-	}, [])
+  const [account, isConnected, loadAccounts] = useWeb3()
 
   return (
     <div className="App">
---
+++ src/hooks/web3.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react"
+import Web3 from 'web3';
+
+export const useWeb3 = () => {
+  const [account, setAccount] = useState('0x0')
+  const [isConnected, setIsConnected] = useState(false)
+  // const [loading, setLoading] = useState(true)
+  const loadAccounts = async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(accounts[0])
+      setIsConnected(true)
+    } catch (err) {
+      setIsConnected(false)
+    }
+  }
+
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+
+      await loadAccounts()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected.')
+    }
+  }
+
+  loadWeb3()
+  
+  const setupWalletListeners = () => {
+    window.ethereum.on('accountsChanged', (accounts) => {
+      //Time to reload your interface with accounts[0]!
+      if (accounts.length > 0) {
+        setAccount(accounts[0])
+        setIsConnected(true)
+      } else {
+        setAccount('0x0')
+        setIsConnected(false)
+      }
+    })
+  }
+
+  useEffect(() => {
+		setupWalletListeners()
+	}, [])
+
+  return [account, isConnected, loadAccounts]
+}
